f0c845fcccc252c1b140febf3a6caddee0b1749f
Antonella Tintilay Raquel <antotintilay6@gmail.com>
2026-05-01 22:18:04 -0300

    Busqueda dinamica

--- js/api.js
+++ js/api.js
@@ -1,28 +1,8 @@
 const contenedor = document.getElementById("contenedor");
 const estado = document.getElementById("estado");
+const input = document.getElementById("busqueda");
 
-const obtenerPersonajes = async () => {
-    try {
-        estado.textContent = "Cargando...";
-
-        const response = await fetch("https://rickandmortyapi.com/api/character");
-
-        if (!response.ok) {
-            throw new Error("Error en la API");
-        }
-
-        const data = await response.json();
-
-        estado.textContent = "";
-
-        mostrarPersonajes(data.results);
-
-    } catch (error) {
-        estado.textContent = "Error al cargar los datos";
-        console.error(error);
-    }
-};
-
+let timeout;
 const mostrarPersonajes = (lista) => {
     contenedor.innerHTML = lista.map(personaje => `
         <div class="card">
@@ -32,5 +12,49 @@ const mostrarPersonajes = (lista) => {
         </div>
     `).join("");
 };
+const obtenerPersonajes = async (url) => {
+    try {
+        estado.textContent = "Cargando...";
 
-obtenerPersonajes();
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Error en la API");
+        }
+
+        const data = await response.json();
+
+        if (!data.results || data.results.length === 0) {
+            estado.textContent = "No se encontraron resultados";
+            contenedor.innerHTML = "";
+            return;
+        }
+
+        estado.textContent = "";
+        mostrarPersonajes(data.results);
+
+    } catch (error) {
+        estado.textContent = "Error de red o API";
+        contenedor.innerHTML = "";
+    }
+};
+
+input.addEventListener("input", (e) => {
+    const valor = e.target.value.trim();
+
+    clearTimeout(timeout);
+    if (valor.length < 3) {
+        estado.textContent = "Escribí al menos 3 caracteres";
+        contenedor.innerHTML = "";
+        return;
+    }
+
+    estado.textContent = "Buscando...";
+
+    timeout = setTimeout(() => {
+        obtenerPersonajes(
+            `https://rickandmortyapi.com/api/character/?name=${valor}`
+        );
+    }, 500);
+});
+obtenerPersonajes("https://rickandmortyapi.com/api/character");
